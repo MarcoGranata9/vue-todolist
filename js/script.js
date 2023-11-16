45652a8beac8b18508ab3533b63b13840844b1b3
MarcoGranata9 <granatam9900@gmail.com>
2023-11-16 06:14:16 +0100

    add: bonus 3

--- js/script.js
+++ js/script.js
@@ -3,17 +3,23 @@ const {createApp} = Vue
 createApp ({
     data() {
         return {
-            todoElem: "",
+            todoElem: {
+                text: "",
+                done: false
+            },
             todoArray: [],
         }
     },
     methods: {
         addTodo: function() {
-            this.todoArray.push(this.todoElem)
-            this.todoElem = ""
+            this.todoArray.push({...this.todoElem})   
+            this.todoElem.text = ""
         },
         removeTodo: function(index) {
             this.todoArray.splice(index, 1)
+        },
+        lineThroughClick: function(index) {
+            this.todoArray[index].done = !this.todoArray[index].done
         }
     }
 }).mount("#app")
